d8d58d375d525872bc4837e70cd5cc31d28e7f34
danedngn <danedngn@gmail.com>
2022-11-13 22:12:23 -0500

    npm install

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+  origin: process.env.CLIENT_ORIGIN || "http://localhost"
 };
 
 app.use(cors(corsOptions));
